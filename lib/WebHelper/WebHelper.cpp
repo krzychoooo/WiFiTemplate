88f340f5cb59f9eacebc244ce2198608f72d9410
krzychooo <krzysztofklimas@interia.pl>
2025-12-17 12:35:24 +0100

    after main from ESP32_WiFi_Manager.ino

---
+++ lib/WebHelper/WebHelper.cpp
@@ -0,0 +1,17 @@
+
+#include "WebHelper.h"
+
+
+// Replaces placeholder with LED state value
+String processor(const String& var) {
+  if(var == "STATE") {
+    if(digitalRead(ledPin)) {
+      ledState = "ON";
+    }
+    else {
+      ledState = "OFF";
+    }
+    return ledState;
+  }
+  return String();
+}
---
+++ lib/WebHelper/WebHelper.h
@@ -0,0 +1,6 @@
+#ifndef EEDBEB64_97E2_4FD8_B11F_DF4719F9F3E4
+#define EEDBEB64_97E2_4FD8_B11F_DF4719F9F3E4
+
+#include <Arduino.h>
+String processor(const String& var);
+#endif /* EEDBEB64_97E2_4FD8_B11F_DF4719F9F3E4 */
