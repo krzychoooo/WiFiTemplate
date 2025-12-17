a4ed09425a904706f07df1bc263dd56f354a310b
krzychooo <krzysztofklimas@interia.pl>
2025-12-17 13:28:34 +0100

    procesor move to webhelper

--- lib/WebHelper/WebHelper.cpp
+++ lib/WebHelper/WebHelper.cpp
@@ -5,13 +5,13 @@
 // Replaces placeholder with LED state value
 String processor(const String& var) {
   if(var == "STATE") {
-    if(digitalRead(ledPin)) {
-      ledState = "ON";
-    }
-    else {
-      ledState = "OFF";
-    }
-    return ledState;
+    // if(digitalRead(ledPin)) {
+    //   ledState = "ON";
+    // }
+    // else {
+    //   ledState = "OFF";
+    // }
+    // return ledState;
   }
   return String();
 }
